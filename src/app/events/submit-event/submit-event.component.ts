35d699da63ba075a8792f76108e5972df8fdb926
Laura Weston <l.weston@outlook.com>
2024-09-09 21:17:42 +0100

    remove submit event until its ready

--- src/app/events/submit-event/submit-event.component.ts
+++ src/app/events/submit-event/submit-event.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-submit-event',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, FormsModule],
   templateUrl: './submit-event.component.html',
   styleUrl: './submit-event.component.scss'
 })
