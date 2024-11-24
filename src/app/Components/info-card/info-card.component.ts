24dc57a1166e578b2bbae13587c89ce230db1cba
Laura Weston <l.weston@outlook.com>
2024-11-24 12:08:01 +0000

    created info-card shared component

---
+++ src/app/Components/info-card/info-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { GroupInfo } from '../../home/groups/groups.constants';
+
+@Component({
+  selector: 'app-info-card',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+  ],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.scss'
+})
+export class InfoCardComponent {
+  @Input() group!: GroupInfo;
+}
