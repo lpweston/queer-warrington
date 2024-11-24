75ad1b15285aca586ca26ca3e17c353eab67d0a1
Laura Weston <l.weston@outlook.com>
2024-11-24 12:08:24 +0000

    using shared info card in groups

--- src/app/home/groups/groups.component.ts
+++ src/app/home/groups/groups.component.ts
@@ -1,11 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { GroupInfo, SocialInfo, groups, socials, companies } from './groups.constants';
+import { GroupInfo, SocialInfo, groups, socials, companies, supportServices } from './groups.constants';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { InfoCardComponent } from '../../Components/info-card/info-card.component';
 
 @Component({
   selector: 'app-groups',
@@ -16,13 +17,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatListModule,
     MatIconModule,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    InfoCardComponent
   ],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.scss'
 })
 export class GroupsComponent {
   groups: GroupInfo[] = groups;
+  supportServices: GroupInfo[] = supportServices;
   socials: SocialInfo[] = socials;
   companies: SocialInfo[] = companies;
   readonly dialog = inject(MatDialog);
--- src/app/home/groups/groups.constants.ts
+++ src/app/home/groups/groups.constants.ts
@@ -5,9 +5,9 @@ export type GroupInfo = {
     icon: string,
     imagePath: string,
     description: string
-    link: string
+    links: SocialInfo[]
   }
-  
+
 export type SocialInfo = {
     name: string,
     link: string,
@@ -24,8 +24,30 @@ export const groups: GroupInfo[] = [
       description: `This group started to be a queer safe place in the warrington area. It's a group for everyone: the shes/zies/theys/hes/zers and everyone in-between who is part of the LGBTQIA+ community.<br/><br/>
           We meet up (at a minimum) on the last Saturday of each month at the Geek Retreat Warrington at 11.30 am. And are open to adding different locations and different activities on top of this.
           This group is for anyone aged 18 or over.`,
-      link: 'https://linktr.ee/queercoffee'
-    },
+      links: [
+        {
+          name: "",
+          link: 'https://linktr.ee/queercoffee',
+          icon: '/assets/icons/linktree.png',
+          iconAlt: 'Queer Coffee Linktree',
+        },
+        { 
+          name: "",
+          link: 'https://www.facebook.com/groups/warringtonqueercoffee',
+          icon: '/assets/icons/fb-wh.png',
+          iconAlt: 'Queer Coffee Facebook'
+        },
+        { 
+          name: "",
+          link: 'https://discord.com/invite/4xFx7KuGH5',
+          icon: '/assets/icons/discord-mark-white.svg',
+          iconAlt: 'Queer Coffee Discord'
+        },
+      ]
+    }
+  ]
+
+  export const supportServices: GroupInfo[] = [
     {
       name: "LovingMe",
       subtitle: 'Support Service',
@@ -34,10 +56,34 @@ export const groups: GroupInfo[] = [
       description: `We work with trans / non-binary adults in England who are experiencing domestic abuse or sexual violence. 
       We offer one-to-one support and advocacy, and have a refuge in Lancashire which is specifically for trans / non-binary people.
       We have a small team of staff all of whom are trans or non-binary ourselves.<br/><br/>
-      Website: <a href='lovingme.uk'>lovingme.uk</a><br/>
       Call: 07902 478958<br/>
       Email: help@lovingme.uk`,
-      link: 'https://linktr.ee/lovingme_uk'
+      links: [
+        {
+          name: "",
+          link: 'https://www.lovingme.uk/',
+          icon: 'home',
+          iconAlt: 'Loving Me Website',
+        },
+        {
+          name: "",
+          link: 'https://linktr.ee/lovingme_uk',
+          icon: '/assets/icons/linktree.png',
+          iconAlt: 'Queer Coffee Linktree',
+        },
+        { 
+          name: "",
+          link: 'https://www.facebook.com/lovingme.uk?_rdr',
+          icon: '/assets/icons/fb-wh.png',
+          iconAlt: 'Loving Me Facebook'
+        },
+        { 
+          name: "",
+          link: 'https://www.instagram.com/lovingme_uk/',
+          icon: '/assets/icons/Instagram_Glyph_White.svg',
+          iconAlt: 'Loving Me Instagram'
+        },
+      ]
     },
     {
       name: "Trans Without Abuse",
@@ -47,11 +93,17 @@ export const groups: GroupInfo[] = [
       description: `Cheshire Without Abuse (CWA) offer support across the whole of Cheshire. Our main domestic abuse service is with CWA 
       and offers support to everyone; inclusive of all of the LGBTQIA+ communities.
       Trans without Abuse specifically supports trans and non-binary people, across Cheshire, that have been affected by domestic abuse.
-      We also offer professional training in trans and non-binary domestic abuse practice, as well as bespoke programmes for domestic abuse recovery.<br/><br/>
-      Website: <a href='www.transwithoutabuse.org.uk'>www.transwithoutabuse.org.uk</a><br/>
+      We also offer professional training in trans and non-binary domestic abuse practice, as well as bespoke programmes for domestic abuse recovery.<br/><br/
       Call: 01270 972 208<br/>
       Email: debbie.c@mycwa.org.uk`,
-      link: 'www.transwithoutabuse.org.uk'
+      links: [
+        {
+          name: "",
+          link: 'https://www.transwithoutabuse.org.uk',
+          icon: 'home',
+          iconAlt: 'Trans without abuse Website',
+        },
+      ]
     }
   ]
 
@@ -59,19 +111,19 @@ export const groups: GroupInfo[] = [
     {
       name: 'LGBT group 1',
       link: 'https://www.google.com',
-      icon: '/assets/images/fb.png',
+      icon: '/assets/icons/fb.png',
       iconAlt: "facebook"
     },
     {
       name: 'LGBT group 2',
       link: 'https://www.google.com',
-      icon: '/assets/images/fb.png',
+      icon: '/assets/icons/fb.png',
       iconAlt: "facebook"
     },
     {
       name: 'LGBT group 3',
       link: 'https://www.google.com',
-      icon: '/assets/images/fb.png',
+      icon: '/assets/icons/fb.png',
       iconAlt: "facebook"
     }
   ]
