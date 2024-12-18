87fe6b0414629c99979e3e4059a5977538cc95bb
Laura Weston <l.weston@outlook.com>
2024-12-18 17:09:15 +0000

    Initial pride page using D3

--- src/app/pride/pride.component.ts
+++ src/app/pride/pride.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { test, WarringtonLocation } from './pride2024.constants';
 
 @Component({
   selector: 'app-pride',
@@ -7,6 +9,80 @@ import { Component } from '@angular/core';
   templateUrl: './pride.component.html',
   styleUrl: './pride.component.scss'
 })
-export class PrideComponent {
+export class PrideComponent implements OnInit {
+  // Declare the chart dimensions and margins.
+  private width = 1200;
+  private height = 400;
+  private marginTop = 20;
+  private marginRight = 20;
+  private marginBottom = 30;
+  private marginLeft = 150;
 
+  private colors = ["red", "orange", "gold", "green", "blue", "purple"]
+  private colorIdx = 0;
+
+  private startDate = new Date("2024-06-08T08:00");
+  private endDate = new Date("2024-06-09T03:00");
+
+  ngOnInit() {
+    // Declare the x (horizontal position) scale.
+    const x = d3.scaleUtc()
+        .domain([this.startDate , this.endDate])
+        .range([this.marginLeft, this.width - this.marginRight]);
+
+    const locations = Object.values(WarringtonLocation).map(v => v.toString());
+    const y = d3.scaleOrdinal()
+              .domain([...locations])
+              .range(this.getLocationsRange(locations.length)) as d3.ScaleOrdinal<string, number, never>;
+
+    // Create the SVG container.
+    const dataGroup = d3.select("#chart")
+        .attr("width", this.width)
+        .attr("height", this.height);
+        
+        // x-axis
+        dataGroup.append("g")
+        .attr("transform", `translate(0,${this.height - this.marginBottom})`)
+        .call(d3.axisBottom(x));
+        
+        // y-axis
+        dataGroup.append("g")
+        .attr("transform", `translate(${this.marginLeft},0)`)
+        .call(d3.axisLeft(y));
+
+        // data
+        dataGroup.append("g")
+            .attr("class", "bars")
+            .selectAll("rect")
+            .data(test)
+            .join("rect")
+            .attr("x", d => x(d.start))
+            .attr("y", d => y(d.location) -10)
+            .attr("width", d => this.getWidthOfBar(d.start, d.end))
+            .attr("height", 20)
+            .attr("fill", d => this.getNextColor());
+    }
+
+    private getLocationsRange(locationsCount: number): number[] {
+      const space = this.height - this.marginTop;
+      const step = space / locationsCount;
+      const a = [this.marginTop]
+      for (let i = 1; i < locationsCount; i++) {
+        a.push(a[i-1] + step);
+      }
+      return a;
+    }
+
+    private getWidthOfBar(start: Date, end: Date): number {
+      const widthOfMS = this.width / (this.endDate.valueOf() - this.startDate.valueOf());
+      return (end.valueOf() - start.valueOf()) * widthOfMS;
+    }
+
+    private getNextColor() {
+      const color = this.colors[this.colorIdx++];
+      if (this.colorIdx >= this.colors.length) {
+        this.colorIdx = 0;
+      }
+      return color;
+    }
 }
--- src/app/pride/pride2024.constants.ts
+++ src/app/pride/pride2024.constants.ts
@@ -1,13 +1,11 @@
-import { LocationDetail } from "./time.util";
-
   // get these from google api?
   type prideEvent = {
     name: string,
     location: WarringtonLocation,
-    start: string,
-    end: string,
-    description: string,
-    link: string
+    start: Date,
+    end: Date,
+    description?: string,
+    link?: string
   }
 
   export enum WarringtonLocation {
@@ -23,230 +21,180 @@ import { LocationDetail } from "./time.util";
     pyramid = "Pyramid Art Centre"
   }
 
-  export const test: LocationDetail[] = [
+  export const test: prideEvent[] = [
     {
-        name: WarringtonLocation.market,
-        events: [
-            // {
-            //     name: "Kiki Inferno and Friends Drag Cabaret",
-            //     //value: 10
-            //     start:   new Date("2024-06-07T20:00"),
-            //     end:  new Date("2024-06-07T23:00")
-            // },
-            {
-                name: "Live Music",
-                //value: 20,
-                start:   new Date("2024-06-08T20:00"),
-                end:  new Date("2024-06-08T24:00")
-
-            }
-        ]
+        location: WarringtonLocation.market,
+        name: "Kiki Inferno and Friends Drag Cabaret",
+        start:   new Date("2024-06-07T20:00"),
+        end:  new Date("2024-06-07T23:00"),
     },
     {
-        name: WarringtonLocation.university,
-        events: [
-            // {
-            //     name: "Place to Pause Conversation Space",
-            //     //value: 30,
-            //     start: new Date("2024-06-08T10:00"),
-            //     end: new Date("2024-06-08T15:00")
-            // },
-            // {
-            //     name: "I Put My Love In Your Hands",
-            //     //value: 30,
-            //     start: new Date("2024-06-08T10:00"),
-            //     end: new Date("2024-06-08T16:00")
-            // },
-            {
-                name: "Male Presenting: Creating Fiction from Facts",
-                //value: 30,
-                start: new Date("2024-06-08T10:30"),
-                end: new Date("2024-06-08T11:00")
-            },
-            {
-                name: "Tour",
-                //value: 30,
-                start: new Date("2024-06-08T11:30"),
-                end: new Date("2024-06-08T12:00")
-            },
-            {
-                name: "Our Stonewall: Remembering the Camp Revolt Against Section 28",
-                //value: 30,
-                start: new Date("2024-06-08T12:30"),
-                end: new Date("2024-06-08T13:00")
-            },
-            {
-                name: "Transgender and Transition: Lived Experience from Amanda Hilton",
-                //value: 30,
-                start: new Date("2024-06-08T13:15"),
-                end: new Date("2024-06-08T13:45")
-            },
-            // {
-            //     name: "Get set for Pride",
-            //     //value: 30,
-            //     start: new Date("2024-06-08T10:00"),
-            //     end: new Date("2024-06-08T11:00")
-            // },
-        ]
+        location: WarringtonLocation.market,
+        name: "Live Music",
+        start:   new Date("2024-06-08T20:00"),
+        end:  new Date("2024-06-08T24:00")
     },
     {
-        name: WarringtonLocation.library,
-        events: [
-            {
-                name: "LGBT+ book event",
-                //value: 30,
-                start: new Date("2024-06-08T12:30"),
-                end: new Date("2024-06-08T13:30")
-            },
-            // {
-            //     name: "Activities",
-            //     //value: 30,
-            //     start: new Date("2024-06-08T11:00"),
-            //     end: new Date("2024-06-08T14:00")
-            // },
-        ]
+        location: WarringtonLocation.university,
+        name: "Place to Pause Conversation Space",
+        start: new Date("2024-06-08T10:00"),
+        end: new Date("2024-06-08T15:00")
     },
     {
-        name: WarringtonLocation.goldenSquare,
-        events: [
-            {
-                name: "Information and support hub",
-                //value: 30,
-                start: new Date("2024-06-08T11:00"),
-                end: new Date("2024-06-08T18:00")
-            },
-        ]
+        location: WarringtonLocation.university,
+        name: "I Put My Love In Your Hands",
+        start: new Date("2024-06-08T10:00"),
+        end: new Date("2024-06-08T16:00")
     },
     {
-        name: WarringtonLocation.oldMarketSquare,
-        events: [
-            // {
-            //     name: "Crafty Fox Glitter and Face Painting",
-            //     //value: 30,
-            //     start: new Date("2024-06-08T11:00"),
-            //     end: new Date("2024-06-08T23:00")
-            // },
-            {
-                name: "Dance performances",
-                //value: 30,
-                start: new Date("2024-06-08T12:10"),
-                end: new Date("2024-06-08T12:25")
-            },
-            {
-                name: "Queerera Fashion Show",
-                //value: 30,
-                start: new Date("2024-06-08T12:25"),
-                end: new Date("2024-06-08T12:50")
-            },
-            {
-                name: "Variety Performance",
-                //value: 30,
-                start: new Date("2024-06-08T12:50"),
-                end: new Date("2024-06-08T13:25")
-            },
-            {
-                name: "Queerera Fashion Show",
-                //value: 30,
-                start: new Date("2024-06-08T13:25"),
-                end: new Date("2024-06-08T14:05")
-            },
-            {
-                name: "Variety performances",
-                //value: 30,
-                start: new Date("2024-06-08T14:05"),
-                end: new Date("2024-06-08T14:20")
-            },
-            {
-                name: "Dance performances",
-                //value: 30,
-                start: new Date("2024-06-08T14:20"),
-                end: new Date("2024-06-08T14:30")
-            },
-            {
-                name: "Silent disco",
-                //value: 30,
-                start: new Date("2024-06-08T14:30"),
-                end: new Date("2024-06-08T16:00")
-            },
-            {
-                name: "Glitter",
-                //value: 30,
-                start: new Date("2024-06-08T18:00"),
-                end: new Date("2024-06-09T02:00")
-            },
-        ]
+        location: WarringtonLocation.university,
+        name: "Male Presenting: Creating Fiction from Facts",
+        start: new Date("2024-06-08T10:30"),
+        end: new Date("2024-06-08T11:00")
     },
     {
-        name: WarringtonLocation.timeSquare,
-        events: [
-            {
-                name: "Pre-parade speeches",
-                //value: 30,
-                start: new Date("2024-06-08T11:00"),
-                end: new Date("2024-06-08T11:30")
-            },
-            {
-                name: "Parade",
-                //value: 30,
-                start: new Date("2024-06-08T11:30"),
-                end: new Date("2024-06-08T12:00")
-            },
-        ]
+        location: WarringtonLocation.university,
+        name: "Tour",
+        start: new Date("2024-06-08T11:30"),
+        end: new Date("2024-06-08T12:00")
     },
     {
-        name: WarringtonLocation.youthZone,
-        events: [
-            {
-                name: "Juniors Rainbow Party",
-                //value: 30,
-                start: new Date("2024-06-08T11:30"),
-                end: new Date("2024-06-08T13:00")
-            },
-            {
-                name: "Seniors Rainbow Party",
-                //value: 30,
-                start: new Date("2024-06-08T16:00"),
-                end: new Date("2024-06-08T22:00")
-            },
-        ]
+        location: WarringtonLocation.university,
+        name: "Our Stonewall: Remembering the Camp Revolt Against Section 28",
+        start: new Date("2024-06-08T12:30"),
+        end: new Date("2024-06-08T13:00")
     },
     {
-        name: WarringtonLocation.hive,
-        events: [
-            {
-                name: "Pride at the Hive",
-                //value: 30,
-                start: new Date("2024-06-08T19:30"),
-                end: new Date("2024-06-08T24:00")
-            },
-        ]
+        location: WarringtonLocation.university,
+        name: "Transgender and Transition: Lived Experience from Amanda Hilton",
+        start: new Date("2024-06-08T13:15"),
+        end: new Date("2024-06-08T13:45")
     },
-    // {
-    //     name: WarringtonLocation.pavilion,
-    //     events: [
-    //         {
-    //             name: "Coffee and Cakes",
-    //             //value: 30,
-    //             start: new Date("2024-06-09T13:00"),
-    //             end: new Date("2024-06-09T16:00")
-    //         },
-    //     ]
-    // },
-    // {
-    //     name: WarringtonLocation.pyramid,
-    //     events: [
-    //         {
-    //             name: "Chicken",
-    //             //value: 30,
-    //             start: new Date("2024-06-11T19:00"),
-    //             end: new Date("2024-06-11T23:00")
-    //         },
-    //         {
-    //             name: "Chicken",
-    //             //value: 30,
-    //             start: new Date("2024-06-12T19:00"),
-    //             end: new Date("2024-06-12T23:00")
-    //         },
-    //     ]
-    // },
+    {
+        location: WarringtonLocation.university,
+        name: "Get set for Pride",
+        start: new Date("2024-06-08T10:00"),
+        end: new Date("2024-06-08T11:00")
+    },
+    {
+        location: WarringtonLocation.library,
+        name: "LGBT+ book event",
+        start: new Date("2024-06-08T12:30"),
+        end: new Date("2024-06-08T13:30")
+    },
+    {
+        location: WarringtonLocation.library,
+        name: "Activities",
+        start: new Date("2024-06-08T11:00"),
+        end: new Date("2024-06-08T14:00")
+    },
+    {
+        location: WarringtonLocation.goldenSquare,
+        name: "Information and support hub",
+        start: new Date("2024-06-08T11:00"),
+        end: new Date("2024-06-08T18:00")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Crafty Fox Glitter and Face Painting",
+        start: new Date("2024-06-08T11:00"),
+        end: new Date("2024-06-08T23:00")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Dance performances",
+        start: new Date("2024-06-08T12:10"),
+        end: new Date("2024-06-08T12:25")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Queerera Fashion Show",
+        start: new Date("2024-06-08T12:25"),
+        end: new Date("2024-06-08T12:50")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Variety Performance",
+        start: new Date("2024-06-08T12:50"),
+        end: new Date("2024-06-08T13:25")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Queerera Fashion Show",
+        start: new Date("2024-06-08T13:25"),
+        end: new Date("2024-06-08T14:05")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Variety performances",
+        start: new Date("2024-06-08T14:05"),
+        end: new Date("2024-06-08T14:20")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Dance performances",
+        start: new Date("2024-06-08T14:20"),
+        end: new Date("2024-06-08T14:30")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Silent disco",
+        start: new Date("2024-06-08T14:30"),
+        end: new Date("2024-06-08T16:00")
+    },
+    {
+        location: WarringtonLocation.oldMarketSquare,
+        name: "Glitter",
+        start: new Date("2024-06-08T18:00"),
+        end: new Date("2024-06-09T02:00")
+    },
+    {
+        location: WarringtonLocation.timeSquare,
+        name: "Pre-parade speeches",
+        start: new Date("2024-06-08T11:00"),
+        end: new Date("2024-06-08T11:30")
+    },
+    {
+        location: WarringtonLocation.timeSquare,
+        name: "Parade",
+        start: new Date("2024-06-08T11:30"),
+        end: new Date("2024-06-08T12:00")
+    },
+    {
+        location: WarringtonLocation.youthZone,
+        name: "Juniors Rainbow Party",
+        start: new Date("2024-06-08T11:30"),
+        end: new Date("2024-06-08T13:00")
+    },
+    {
+        location: WarringtonLocation.youthZone,
+        name: "Seniors Rainbow Party",
+        start: new Date("2024-06-08T16:00"),
+        end: new Date("2024-06-08T22:00")
+    },
+    {
+        location: WarringtonLocation.hive,
+        name: "Pride at the Hive",
+        
+        start: new Date("2024-06-08T19:30"),
+        end: new Date("2024-06-08T24:00")
+    },
+    {
+        location: WarringtonLocation.pavilion,
+        name: "Coffee and Cakes",
+        start: new Date("2024-06-09T13:00"),
+        end: new Date("2024-06-09T16:00")
+    },
+    {
+        location: WarringtonLocation.pyramid,
+        name: "Chicken",
+        start: new Date("2024-06-11T19:00"),
+        end: new Date("2024-06-11T23:00")
+    },
+    {
+        location: WarringtonLocation.pyramid,
+        name: "Chicken",
+        start: new Date("2024-06-12T19:00"),
+        end: new Date("2024-06-12T23:00")
+    },
 ];
