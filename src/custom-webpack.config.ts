57646c00e42111027fc63151e47f62e1f50727dc
Laura Weston <l.weston@outlook.com>
2025-01-25 18:42:17 +0000

    systemvars true to make env work on netlify

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,4 +1,7 @@
 const Dotenv = require('dotenv-webpack');
 module.exports = {
-  plugins: [new Dotenv()],
+  plugins: [new Dotenv({
+    path: `.env`,
+    systemvars: true, //Set to true if you would rather load all system variables as well (useful for CI purposes)
+  })],
 };
