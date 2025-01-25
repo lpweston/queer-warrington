5a9cc20928f26b4ab3ca234ea27d5fb8d107b2e1
Laura Weston <l.weston@outlook.com>
2025-01-25 18:50:19 +0000

    remove path from env

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,7 +1,6 @@
 const Dotenv = require('dotenv-webpack');
 module.exports = {
   plugins: [new Dotenv({
-    path: `./.env`,
     systemvars: true, //Set to true if you would rather load all system variables as well (useful for CI purposes)
   })],
 };
