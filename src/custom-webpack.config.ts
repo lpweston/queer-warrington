b9c67809b7bc554c25d11c75563766bc9da069e8
Laura Weston <l.weston@outlook.com>
2025-01-25 18:45:23 +0000

    updated path to env

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,7 +1,7 @@
 const Dotenv = require('dotenv-webpack');
 module.exports = {
   plugins: [new Dotenv({
-    path: `.env`,
+    path: `./.env`,
     systemvars: true, //Set to true if you would rather load all system variables as well (useful for CI purposes)
   })],
 };
