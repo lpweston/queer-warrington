34ff7778c4ea858249c499fedac0cd918b6763ac
Laura Weston <l.weston@outlook.com>
2025-01-25 19:04:37 +0000

    undo previous change

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,8 +1,5 @@
 const Dotenv = require('dotenv-webpack');
 
-const fs = require('fs')
-fs.writeFileSync('./.env', `calendarAddress=${process.env["calendarAddress"]}\ncalendarAPI=${process.env["calendarAPI"]}\n`)
-
 module.exports = {
   plugins: [new Dotenv({systemvars: true})],
 };
