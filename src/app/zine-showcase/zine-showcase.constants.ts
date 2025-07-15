4c02b8319eb7b21ddc78accf007811ba87e52cb9
Laura Weston <l.weston@outlook.com>
2025-07-15 19:31:45 +0100

    link to zines on home page

--- src/app/zine-showcase/zine-showcase.constants.ts
+++ src/app/zine-showcase/zine-showcase.constants.ts
@@ -10,50 +10,50 @@ export const pdfSrc: pdfInfo[]= [
       file: "LydiaFaye",
       name: "Lydia Faye",
       altText: `1: "Growing up confused about my sexuality, and not fitting in, was rough - I hated school, the kids were brutal." 
-                Sad person with green and pink hair wearing black clothes being mocked by a crowd saying things like: Gay, Freak, Weirdo, Lesbo, Mosher and Gross.<br/>
+                Sad person with green and pink hair wearing black clothes being mocked by a crowd saying things like: Gay, Freak, Weirdo, Lesbo, Mosher and Gross.
                 2: "I lost my way as a teen - drank, smoked, messed around - anything to make things better, but, it didn't work."
-                Person is sitting on a bed, crying next to alcohol and cigarettes.<br/>
+                Person is sitting on a bed, crying next to alcohol and cigarettes.
                 3: "Then I had you, and everything changed. All the hate and fear turned to pure love." 
-                Blond person in bed with a newborn baby, both look content and there are love hearts.<br/>
+                Blond person in bed with a newborn baby, both look content and there are love hearts.
                 4: "Now I love who I am. I love my family. I love that I am loved, and they are loved. I love raising them to feel safe, secure and accepted for who they are. 
-                I will raise them with all the love I can give. We are a happy, loving family. Queer, straight, it doesn't matter. Family is family. Love is love."<br/>
+                I will raise them with all the love I can give. We are a happy, loving family. Queer, straight, it doesn't matter. Family is family. Love is love."
                 Happy family, including an adult with black and green hair with a pansexual flag pin, an adult with short brown hair and glasses holding a baby,
                 a teen with long brown hair, dip-dyed with pink and blue, and a child with long blond hair.`
     },
     {
       file: "CameronG",
       name: "Cameron G",
-      altText: `1.Heart with the phrase “Love is Love” inside it.<br/>
-                2.Infinity symbol above the word “Autism”.<br/>
-                3.“I am a barista and I love coffee,” said above a steaming cup of coffee.<br/>
+      altText: `1.Heart with the phrase “Love is Love” inside it.
+                2.Infinity symbol above the word “Autism”.
+                3.“I am a barista and I love coffee,” said above a steaming cup of coffee.
                 4.“Love Queer people & Going to Pride. Coming out to some of my family and I was a bit scared but they were very accepting because
-                I have lesbian cousins who have been married for years and they were a great help.”<br/>
-                5.Zoomed out pencil drawing of people with their arms up going down a rollercoaster.<br/>
-                6.“Trans Rights,” written in a death metal font.<br/>
+                I have lesbian cousins who have been married for years and they were a great help.”
+                5.Zoomed out pencil drawing of people with their arms up going down a rollercoaster.
+                6.“Trans Rights,” written in a death metal font.
                 7.Large pencil drawing of a butterfly flying upwards.`
     },
     {
       file: "Evie",
       name: "Evie",
       altText: `1.“When in primary school I used to get made fun of for reasons I didn’t know.” Pictured: Young girl, Evie, with brown ponytails tied with purple
-                baubles wearing a purple cardigan over a school uniform being laughed at.<br/>
-                2.“I use to go home crying, not being able to sleep at night. No matter what I did I wasn’t good enough.” Pictured: Evie wearing pink crying on a blue bed.<br/>
-                3.“But one day I met my best friend and everything changed.” Pictured: A person with long brown hair and round glasses smiling with Evie.<br/>
-                4.“I felt more and more confident in my self!” Pictured: Evie wearing a light green top with her arms up smiling happily.<br/>
+                baubles wearing a purple cardigan over a school uniform being laughed at.
+                2.“I use to go home crying, not being able to sleep at night. No matter what I did I wasn’t good enough.” Pictured: Evie wearing pink crying on a blue bed.
+                3.“But one day I met my best friend and everything changed.” Pictured: A person with long brown hair and round glasses smiling with Evie.
+                4.“I felt more and more confident in my self!” Pictured: Evie wearing a light green top with her arms up smiling happily.
                 5.“As I went on I figured out myself out a bit more and ignored the horrible insults.” Pictured: Evie now in two space buns wearing a top in the bisexual flag
-                colours (pink, purple, blue) smiling while ignoring insults.<br/>
+                colours (pink, purple, blue) smiling while ignoring insults.
                 6.“I also started getting extra help.” Pictured: Evie with her hair down, wearing a pink top, looking sad. A woman with a blonde updo wearing a purple dress asking
-                her “Why?”.<br/>
-                7.“Now I’m happy in my self!” Pictured: Evie wearing her bisexual flag top, smiling with her hair down and eye makeup on.<br/>`
+                her “Why?”.
+                7.“Now I’m happy in my self!” Pictured: Evie wearing her bisexual flag top, smiling with her hair down and eye makeup on.`
     },
     {
       file: "James",
       name: "James",
-      altText: `1.”I am autistic and bisexual. I always have been. Community is difficult to find. Work home friends family.”<br/>
-                2.”It doesn’t just happen and it doesn’t come easily. It takes constant effort which I can’t easily.”<br/>
-                3.A steaming cup of coffee with a Q inside it. Behind it, the words “Warrington Queer Coffee” repeating in a row of 3,<br/>
-                the colours of which being 1.blue, purple and pink 2.rainbow 3.black and grey. “The effort is worth it. I don’t make it easy for myself.<br/>
-                I am proud of what I have built. It doesn’t have to be perfect, Community is bigger than any one of us. I am proud of who I have become.<br/>
+      altText: `1.”I am autistic and bisexual. I always have been. Community is difficult to find. Work home friends family.”
+                2.”It doesn’t just happen and it doesn’t come easily. It takes constant effort which I can’t easily.”
+                3.A steaming cup of coffee with a Q inside it. Behind it, the words “Warrington Queer Coffee” repeating in a row of 3,
+                the colours of which being 1.blue, purple and pink 2.rainbow 3.black and grey. “The effort is worth it. I don’t make it easy for myself.
+                I am proud of what I have built. It doesn’t have to be perfect, Community is bigger than any one of us. I am proud of who I have become.
                 Community is built and we can’t do it alone.”`
     },
     {
@@ -102,48 +102,48 @@ export const pdfSrc: pdfInfo[]= [
     {
       file: "Steph_and_Charlotte",
       name: "Steph and Charlotte",
-      altText: `1: Two simplified portraits. The first of a person with long ginger hair, framed in green. The second of a person with chin length brown hair, framed in orange.<br/>
+      altText: `1: Two simplified portraits. The first of a person with long ginger hair, framed in green. The second of a person with chin length brown hair, framed in orange.
                 2: "Our pride got cancelled" each letter penned in a different rainbow colour. "(but we'd have come anyway)" penned in blue. "(Love from Liverpool!)" penned in red.
-                Below are drawn landmarks from Liverpool: Wheel Of Liverpool, Cunard Building, St John’s Beacon, Liverpool Metropolitan Cathedral and Super Lambanana.<br/>
-                3: "Warrington Queer Coffee" and logo; a cup, with a Q and rainbow colours inside.<br/>
+                Below are drawn landmarks from Liverpool: Wheel Of Liverpool, Cunard Building, St John’s Beacon, Liverpool Metropolitan Cathedral and Super Lambanana.
+                3: "Warrington Queer Coffee" and logo; a cup, with a Q and rainbow colours inside.
                 4: The Liver Bird symbol, and The Warrington Skittles (Guardians) connected by rail tracks. `
     },
     {
       file: "Stevie",
       name: "Stevie",
-      altText: `1: "When I was in primary school I found myself being jealous of the girls". A sign reads "The Park Primary School".<br/>
+      altText: `1: "When I was in primary school I found myself being jealous of the girls". A sign reads "The Park Primary School".
                 2:"When I was in year 9 in secondary school I took part in a play and I was cast as a female vampire and when I put on the dress I felt like myself."
-                A sign reads "Ashley School". A picture of a long sleeved dress.<br/>
+                A sign reads "Ashley School". A picture of a long sleeved dress.
                 3:"In 2018 I came out to my family and I'd be lying if I said I wasn't scared." A wardrobe labelled "My Closet" inside is the trans (blue, pink and white)
-                and bi (blue, pink and purple) flags.<br/>
+                and bi (blue, pink and purple) flags.
                 4:"In 2022 I attended my first ever pride event in Liverpool and I was nervous but I ended up making friends and had an amazing time" A stage with a 
-                large sign reading "Liverpool Pride 2022".<br/>`
+                large sign reading "Liverpool Pride 2022".`
     },
     {
       file: "TheVeryQueerCaterpillar",
       name: "The Very Queer Caterpillar",
-      altText: `1: A purple dotted egg on a leaf.<br/>
-                2: Zoomed in view of the egg thinking "I think I'm ready."<br/>
-                3: Even more zoomed in view of the egg, with cracks across it.<br/>
-                4: A caterpillar surrounded by cracked egg. Caterpillar has question marks around it.<br/>
+      altText: `1: A purple dotted egg on a leaf.
+                2: Zoomed in view of the egg thinking "I think I'm ready."
+                3: Even more zoomed in view of the egg, with cracks across it.
+                4: A caterpillar surrounded by cracked egg. Caterpillar has question marks around it.
                 5: In centre the head of the caterpillar with its mouth open, "Munch" and "Nom" written above it. Surrounding it are colourful leaves each with 
-                different words on them: study, sporty, girl, dumb, geek, masculine, funny, punk, cute, older, sexy, as well as others partly hidden.<br/>
-                6: Caterpillar has heart shaped eye and a smile, looking a a black leaf with a question mark on it.<br/>
+                different words on them: study, sporty, girl, dumb, geek, masculine, funny, punk, cute, older, sexy, as well as others partly hidden.
+                6: Caterpillar has heart shaped eye and a smile, looking a a black leaf with a question mark on it.
                 7:  A butterfly with wings displaying the progressive pride flag and the words "Proud to be who I am"`
     },
     {
       file: "VICKY",
       name: "Vicky",
-      altText: `1: "I grew up in fantasy, magical fantasy worlds where good prevails." Panda reading a book piled up on other books.<br/>
-                2: "Felt like a creature from another world" large panda sat amongst lots of stick figures.<br/>
+      altText: `1: "I grew up in fantasy, magical fantasy worlds where good prevails." Panda reading a book pilled up on other books.
+                2: "Felt like a creature from another world" large panda sat amongst lots of stick figures.
                 3: "A hunter saw and captured me" A large stick figure with arms crossed, next to the panda in a cage, with hands over its eyes.
-                The words "Silenced" and "Controlled".<br/>
+                The words "Silenced" and "Controlled".
                 4: A Form titled "Sexuality" with tick box options "Straight, Gay, Lesbian, Bisexual" Bisexual has a dash in the box.
-                "Too scared to even check a box" Panda looking sad, surrounded by lightning bolts.<br/>
-                5: "Escape" and "rebirth" written above a lotus flower.<br/>
-                6: Panda wearing a helmet, with a sword and shield "He's behind you!!!". "Now I'm not alone". "Now I believe in Magic"<br/>
-                7: "New Beginnings" trail leading to an X.<br/>
-                8: "New friends" Queer Coffee logo; a cup with a Q inside.<br/>`
+                "Too scared to even check a box" Panda looking sad, surrounded by lightning bolts.
+                5: "Escape" and "rebirth" written above a lotus flower.
+                6: Panda wearing a helmet, with a sword and shield "He's behind you!!!". "Now I'm not alone". "Now I believe in Magic"
+                7: "New Beginnings" trail leading to an X.
+                8: "New friends" Queer Coffee logo; a cup with a Q inside.`
     },
     {
       file: "Anonymous_01",
@@ -163,8 +163,8 @@ export const pdfSrc: pdfInfo[]= [
     {
       file: "Anonymous_04",
       name: "Anonymous",
-      altText: `1: Pikmin onion (tripod with red body and a daisy-like flower on top).<br/>
-                2: Red pikmin in the ground with a pride flag, rather than a sprout, sticking out of it.<br/>
+      altText: `1: Pikmin onion (tripod with red body and a daisy-like flower on top).
+                2: Red pikmin in the ground with a pride flag, rather than a sprout, sticking out of it.
                 3: Zoomed out view of pikmin, revealing shocked pointing man (meme image) "Yo! Pride Pikmin!?" Small love heart with a pikmin in it "Gay Icon"`
     },
   ]
